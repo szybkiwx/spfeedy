a0a753961568137d978886e16b400334c670c327
szybkiwx <andrzej.dajnowski@gmail.com>
2013-05-11 00:44:12 +0200

    recapping project layout

---
+++ application/models.py
@@ -0,0 +1,37 @@
+from application import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(), index = True, unique = True)
+    password = db.Column(db.String(256), index = True)
+    salt = db.Column(db.String(16))
+        
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+
+class Category(db.Model):
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(), index = True, unique = True)
+    parent_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    parent = db.relationship('Category', backref = db.backref('categories', lazy='dynamic'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', backref = db.backref('categories', lazy='dynamic'))
+
+class Subscription(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(), index = True)
+    url = db.Column(db.String(), index = True)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship('Category', backref = db.backref('subscriptions', lazy='dynamic'))
+
+
+class Feed(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(), index = True)
+    link = db.Column(db.String(), index = True)
+    description = db.Column(db.String(), index = True)
+    subscription_id = db.Column(db.Integer, db.ForeignKey('subscription.id'))
+    subscription = db.relationship('Subscription', backref = db.backref('feeds', lazy='dynamic'))
---
+++ application/views.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask import render_template
+from application import app
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
