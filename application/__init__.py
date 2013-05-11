d12f15c44508138875b930909ad3606d61c306ed
szybkiwx <andrzej.dajnowski@gmail.com>
2013-05-12 01:52:16 +0200

    forms and login stubs

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 import os
 
 app = Flask(__name__)
@@ -8,4 +9,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///"+ os.path.join(basedir, 'db', 'spfeedy.db')
 db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 from application import models, views
+
+@login_manager.user_loader
+def load_user(userid):
+    return db.session.query(User).filter(User.id == userId).first()
+
+
---
+++ application/forms.py
@@ -0,0 +1,6 @@
+from flask_wtf import Form
+from wtforms import TextField, DataRequired
+
+class LoginForm(Form):
+
+    
